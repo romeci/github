a49a4bd1922c2ff496b57398c763ed02273d6963
Romeci <git@romeci.com.br>
2021-03-29 20:59:50 -0300

    update

---
+++ 01_layout_painel/README.md
@@ -0,0 +1,7 @@
+# 01_layout_painel
+
+Rodar o comando
+
+npm i -D gulp gulp-autoprefixer gulp-concat gulp-htmlmin gulp-minify gulp-minify-css gulp-sass gulp-uglify gulp-watch
+
+rondando na versao 14.16.0 do NODE JS
--- 01_layout_painel/assets/js/ajax/form_painel_socio.js
+++ 01_layout_painel/assets/js/ajax/form_painel_socio.js
@@ -3,6 +3,13 @@ $('#form-painel-socio').submit(function (e) {
 
     var dados = $( this ).serializeArray();
 
-    console.log(dados);
+    var alerta_1 = $('.alerta-submit-form_1');
+    var alerta_2 = $('.alerta-submit-form_2');
+    var alerta_3 = $('.alerta-submit-form_3');
+
+   $(alerta_1).html('Mensagem de Orientação!').addClass('alerta-submit-form-orientacao');
+   $(alerta_2).html('Mensagem de Concluído!').addClass('alerta-submit-form-concluido');
+   $(alerta_3).html('Mensagem de Erro!').addClass('alerta-submit-form-erro');
     
+      
 });
